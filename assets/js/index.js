12469b8934646a937710a7312aceb05ce46905e0
ImVovanchik <186074825+ImVovanchik@users.noreply.github.com>
2025-03-17 10:20:44 +0200

    Добавил время в панель задач и перенёс кнопку выхода в меню пуск

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,3 +64,15 @@ function toggleMenu() {
         console.debug("[DEBUG] Меню открыто.");
     }
 }
+
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('time').textContent = `${hours}:${minutes}`;
+}
+window.addEventListener('load', function() {
+    updateTime();
+});
+setInterval(updateTime, 1000);
